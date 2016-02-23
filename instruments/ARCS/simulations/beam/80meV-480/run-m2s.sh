e8434bf13c205fcee71cd897feb188a105aae07d
Jiao Lin <linjiao@ornl.gov>
2016-02-23 11:00:27 -0500

    added  instruments/ARCS/simulations/beam from old resources repo

---
+++ instruments/ARCS/simulations/beam/80meV-480/run-m2s.sh
@@ -0,0 +1,1 @@
+arcs_moderator2sample -ncount=1000000000.0 -buffer_size=100000000 --output-dir=_m2sout -moderator.S_filename=/SNS/users/linjiao/release-builders/mcvine/EXPORT/share/mcvine/instruments/ARCS/source_sct521_bu_17_1.dat
